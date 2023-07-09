28c52afba5a178a406ef6cd5957a1348cc8dc494
StocktonManges <stocktonmanges@gmail.com>
2023-07-09 15:40:31 -0600

    Initial commit

---
+++ dry-exercise-1.js
@@ -0,0 +1,10 @@
+/* 
+- The code below manually prints out each value in the animal Array.
+- Refactor the code so it prints each value in the array, following the DRY Principle.
+*/
+
+const animals = ["Reindeer", "Wolf", "Cat"];
+
+for (let i of animals) {
+  console.log(i);
+};
